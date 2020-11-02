49dfd30a25fbcb725760f900e073d45927bc7a9c
xinyongpeng <36496433+xinyongpeng@users.noreply.github.com>
2020-11-02 07:28:05 +0000

    Deploying to gh-pages from  @ 0ab31e370d9f3bd271418d2ecd06e5f1b5dc89de 🚀

---
+++ precache-manifest.40994a844ea04c9ed86545dcf08cd93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c8943fb248302c4baef02cd2a97272",
+    "url": "/github-actions-test/index.html"
+  },
+  {
+    "revision": "c673f9c9716db4de106b",
+    "url": "/github-actions-test/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "46fb897da663154444e6",
+    "url": "/github-actions-test/static/js/2.032a9e91.chunk.js"
+  },
+  {
+    "revision": "c673f9c9716db4de106b",
+    "url": "/github-actions-test/static/js/main.197a74fc.chunk.js"
+  },
+  {
+    "revision": "3e197a94a1aa4daa2c6e",
+    "url": "/github-actions-test/static/js/runtime~main.77bfbec1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
